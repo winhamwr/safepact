c5ebeaf2414be6a40a3b324a794718c54305870c
winhamwr <winhamwr@gmail.com>
2010-12-11 16:33:12 -0500

    Made the home page redirect to the login page

--- safepact/urls.py
+++ safepact/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,8 +12,10 @@ handler500 = "pinax.views.server_error"
 
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {
-        "template": "homepage.html",
+    url(r"^$", redirect_to, {
+        "url": "/account/login/",
+        "permanent": True,
+        "query_strring": True,
     }, name="home"),
     url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     url(r"^admin/", include(admin.site.urls)),
