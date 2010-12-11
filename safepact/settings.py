d284cc78b0f581d89db63ce509e7131c97406391
winhamwr <winhamwr@gmail.com>
2010-12-11 17:13:30 -0500

    Now redirecting the user to the contractor dashboard after login

--- safepact/settings.py
+++ safepact/settings.py
@@ -171,7 +171,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = "/account/login/" # @@@ any way this can be a url name?
-LOGIN_REDIRECT_URLNAME = "what_next"
+LOGIN_REDIRECT_URLNAME = "dashboard_contractor"
 
 EMAIL_CONFIRMATION_DAYS = 2
 EMAIL_DEBUG = DEBUG
